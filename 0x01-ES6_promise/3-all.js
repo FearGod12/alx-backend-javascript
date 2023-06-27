e58b3f4b265b16b49eccceb126d10ab3412780dc
FearGod12 <onyenikechukwudi@gmail.com>
2023-06-27 19:38:05 +0000

    more fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promise1 = uploadPhoto();
-  const promise2 = createUser();
-
-  Promise.all([promise1, promise2])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((all) => {
       console.log(`${all[0].body} ${all[1].firstName} ${all[1].lastName}`);
     })
