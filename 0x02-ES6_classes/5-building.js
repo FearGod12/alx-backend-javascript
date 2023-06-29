72a785254dc557048aad8dc1346879aaa0660347
FearGod12 <onyenikechukwudi@gmail.com>
2023-06-29 22:07:48 +0000

    task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,24 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (new.target !== Building) {
+      this.evacuationWarningMessage();
+    }
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft === 'number') {
+      this._sqft = sqft;
+    } else {
+      throw new Error('TypeError: sqft must be a number');
+    }
+  }
+}
