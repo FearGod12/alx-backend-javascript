8e92eb8c01d8c7f30e7e3429b0e5d103d6f0fc7c
FearGod12 <onyenikechukwudi@gmail.com>
2023-06-30 16:13:42 +0000

    task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,33 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new Error('TypeError: name must be a string');
+    }
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new Error('TypeError: code must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return `[object ${this.code}]`;
+  }
+}
