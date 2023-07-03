ec116bac680bb03e604e4bce5944afb372dee9a9
FearGod12 <onyenikechukwudi@gmail.com>
2023-07-03 22:06:28 +0000

    fix

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8 = new Int8Array(buffer);
-  try {
-    int8[position] = value;
-  } catch (err) {
+  if (position < 0 || position >= int8.length) {
     throw new Error('Position outside range');
+  } else {
+    int8[position] = value;
   }
-  return buffer;
+  return new DataView(buffer);
 }
