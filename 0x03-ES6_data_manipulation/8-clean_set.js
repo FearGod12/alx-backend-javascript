83d8fb3b9e52866c5cda06bdcf18e114ba2ac1e5
FearGod12 <onyenikechukwudi@gmail.com>
2023-07-04 15:44:02 +0000

    task8 fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (!startString || !set || !(set instanceof Set)) {
+  if (!startString || typeof startString !== 'string' || !set || !(set instanceof Set)) {
     return '';
   }
   const string = [];
   for (const each of set) {
     if (each.startsWith(startString) && typeof each === 'string') {
-      const item = each.slice(startString.length);
+      const item = each.substring(startString.length);
       if (item && item !== each) {
         string.push(item);
       }
