d4a8767e0766768fcd2579a9745a3d6b653094eb
Onyenike Chukwudi FearGod <onyenikechukwudi@gmail.com>
2023-07-06 15:11:11 +0100

    task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -13,28 +13,56 @@ const teacher3: Teacher = {
     lastName: 'Doe',
     location: 'London',
     contract: false
-  };
+};
 console.log(teacher3);
-  
-  interface Directors extends Teacher {
-    numberOfReports: number
-  };
 
-  const director1: Directors = {
+interface Directors extends Teacher {
+    numberOfReports: number
+};
+
+const director1: Directors = {
     firstName: 'John',
     lastName: 'Doe',
     location: 'London',
     fullTimeEmployee: true,
     numberOfReports: 17,
-  };
-  console.log(director1);
+};
+console.log(director1);
 
-  interface printTeacherFunction {
+interface printTeacherFunction {
     (firstName: string, lastName: string): string;
-  }
+}
 
-  const printTeacher: printTeacherFunction = (firstName, lastName) =>{
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName.slice(0, 1).toUpperCase()}. ${lastName}`
-  };
+};
 
-  console.log(printTeacher("John", "Doe"));
+console.log(printTeacher("John", "Doe"));
+
+interface Student {
+    workOnHomework(): string,
+    displayName(): string
+}
+
+interface StudentConstructor {
+    firstName: string,
+    lastName: string
+}
+
+class StudentClass implements Student {
+    firstName: string;
+    lastName: string;
+
+    constructor({ firstName, lastName }: StudentConstructor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework() {
+        return 'Currently working';
+    }
+
+    displayName() {
+        return `${this.firstName}, ${this.lastName}`;
+    }
+}
