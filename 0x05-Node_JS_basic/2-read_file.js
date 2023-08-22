383d8d251e86f8fc75728bc390efa63e06fa4b9c
FearGod12 <onyenikechukwudi@gmail.com>
2023-08-22 19:59:06 +0100

    return value

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -23,6 +23,7 @@ function countStudent (path) {
   console.log(`Number of students in CS: ${CS.length}. List: ${CS.join(', ')}`);
 
   console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`);
+  return dbdata
 }
 
 module.exports = countStudent;
