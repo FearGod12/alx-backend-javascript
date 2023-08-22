09fc1a070256ba36fefb0ea7504d2b1fe40353ba
FearGod12 <onyenikechukwudi@gmail.com>
2023-08-22 19:44:01 +0100

    task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,29 +1,31 @@
 const fs = require('fs');
 
-async function countStudent (path) {
+async function countStudents(path) {
   let data;
   try {
     data = await fs.promises.readFile(path, 'utf8');
-  } catch (err) {
+  } catch (error) {
     throw new Error('Cannot load the database');
   }
-  const alldata = data.split('\n');
-  const splitdata = alldata.map((each) => each.split(','))
-    .filter((each) => each.length === 4 && each[0] !== 'firstname');
-  const dbdata = splitdata.map((each) => {
-    return {
-      firstname: each[0],
-      lastname: each[1],
-      age: each[2],
-      field: each[3]
-    };
-  });
-  const SWE = dbdata.filter((student) => student.field === 'SWE').map((student) => student.firstname);
-  const CS = dbdata.filter((student) => student.field === 'CS').map((student) => student.firstname);
-  console.log(`Number of students: ${dbdata.length}`);
-  console.log(`Number of students in CS: ${CS.length}. List: ${CS.join(', ')}`);
-
-  console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`);
+  const students = data.split('\n')
+    .map((student) => student.split(','))
+    .filter((student) => student.length === 4 && student[0] !== 'firstname')
+    .map((student) => ({
+      firstName: student[0],
+      lastName: student[1],
+      age: student[2],
+      field: student[3],
+    }));
+  const csStudents = students
+    .filter((student) => student.field === 'CS')
+    .map((student) => student.firstName);
+  const sweStudents = students
+    .filter((student) => student.field === 'SWE')
+    .map((student) => student.firstName);
+  console.log(`Number of students: ${students.length}`);
+  console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+  console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+  return { students, csStudents, sweStudents };
 }
 
-module.exports = countStudent;
+module.exports = countStudents;
