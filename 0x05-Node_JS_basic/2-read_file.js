e89eda89d7c0af1fb59469498cae4e7d42a37ac9
FearGod12 <onyenikechukwudi@gmail.com>
2023-08-24 11:39:34 +0100

    task 8 fix lint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -23,7 +23,7 @@ function countStudent (path) {
   console.log(`Number of students in CS: ${CS.length}. List: ${CS.join(', ')}`);
 
   console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`);
-  return { dbdata, SWE, CS }
+  return { dbdata, SWE, CS };
 }
 
 module.exports = countStudent;
