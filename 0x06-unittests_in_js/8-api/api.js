1fe16d5431e70f019f5608211a637ff120d2ee31
FearGod12 <onyenikechukwudi@gmail.com>
2023-08-25 22:24:06 +0100

    task 8

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,13 @@
-express = require('express')
+express = require('express');
 
 app = express();
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the payment system');
-})
-
+  res.send('Welcome to the payment system');
+});
 
 app.listen(7865, () => {
-    console.log('API available on localhost port 7865');
+  console.log('API available on localhost port 7865');
 });
 
 module.exports = app;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,18 +1,17 @@
 app = require('./api');
 const chai = require('chai');
-const request = require('request')
+const request = require('request');
 const expect = chai.expect;
 sinon = require('sinon');
-
 
 const url = 'http://localhost:7865';
 
 describe('test suite for the index page', function () {
-    it('correct status and result when / is hit', function (done) {
-        request.get(url, (err, data, body) => {
-            expect(data.statusCode).to.equal(200);
-            expect(body).to.equal('Welcome to the payment system');
-            done();
-        });
+  it('correct status and result when / is hit', function (done) {
+    request.get(url, (err, data, body) => {
+      expect(data.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
     });
+  });
 });
